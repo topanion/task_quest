a84e6d1f94aaed5c931ed6d34570738f8b0aa33c
NGUYEN Jérôme <nj-cs@hotmail.fr>
2023-05-08 18:42:49 +0200

    added a new line in .env to get email from useUser

--- src/pages/api/login.js
+++ src/pages/api/login.js
@@ -12,7 +12,7 @@ export default async function handler(req, res) {
         try {
           const newUser = await db.create("users", req.body.user);
 
-          res.status(201).json({ newUser });
+          res.status(201).json({ data: newUser });
         } catch (error) {
           res.status(500).json({ error: error.message });
         }
