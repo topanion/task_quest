c23ea63be932f2f68c6dde04c38a3b1c9f187105
NGUYEN Jérôme <nj-cs@hotmail.fr>
2023-05-08 18:35:43 +0200

    bug at login, checking what userLogin returns with a console.log

--- src/pages/main.js
+++ src/pages/main.js
@@ -14,6 +14,7 @@ function Main() {
   useEffect(() => {
     const defineUserInfo = () => {
       userService.userLogin(user).then((e) => {
+        console.log(e);
         if (e.data) setUserInfo(e.data[0]);
       });
     };
